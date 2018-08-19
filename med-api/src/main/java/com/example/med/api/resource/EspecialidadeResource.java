88876513ab5f242e80d7b5b9cd81b8eb5d91a0e1
Bruno_Santos <bruncontato2012@gmail.com>
2018-08-19 19:24:21 -0300

    Tratamendo de Mensagems via Json
    Add - Controler Advice
    Messages.properties

--- med-api/src/main/java/com/example/med/api/resource/EspecialidadeResource.java
+++ med-api/src/main/java/com/example/med/api/resource/EspecialidadeResource.java
@@ -6,12 +6,12 @@ import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -31,27 +31,42 @@ public class EspecialidadeResource {
 	@GetMapping
 	public List<Especialidade> listar() {
 		return especialidadeRepository.findAll();
-	
-	}
-	
-	// @ CRIAR Uma Especialidade - Status Code 201 Created
-	@PostMapping
-	@ResponseStatus(HttpStatus.CREATED)
-	public void criar (@RequestBody Especialidade especialidade, HttpServletResponse response) {
-		Especialidade especialidadeSalva = especialidadeRepository.save(especialidade);
-		//Devolvendo o valor criado para o jason 
-		ServletUriComponentsBuilder.fromCurrentRequestUri().path("/[codigo]")
-		.buildAndExpand(especialidadeSalva.getCodigo()).toUri();
-		// aula 3.6 response.setDateHeader("Location", uriToAsc);
-	}
-	
 
-	
-	// Se meu Json trouxer valores Nulos // 204 Not Content 
-	//@GetMapping
-	//public ResponseEntity<?> listar() {
-		//List<Especialidade> especialidades = especialidadeRepository.findAll();
-		//return !especialidades.isEmpty() ? ResponseEntity.ok(especialidades) : ResponseEntity.noContent().build();
-	//}
+	}
+
+	// @ CRIAR Uma Especialidade - Status Code 201 Created //
+	// Criando Valors através do JSON
+	@PostMapping
+	public ResponseEntity<Especialidade> criar(@RequestBody Especialidade especialidade, HttpServletResponse response) {
+		Especialidade especialidadeSalva = especialidadeRepository.save(especialidade);
+
+		// Devolvendo o valor criado para o jason
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/[codigo]")
+				.buildAndExpand(especialidadeSalva.getCodigo()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+
+		// Apos ser Criado uma especialidade, recebe o codigo criado //
+		// @ResponseStatus(HttpStatus.CREATED), Retornando os valores da API;
+		return ResponseEntity.created(uri).body(especialidadeSalva);
+
+	}
+
+	// Buscando a Especialidade pelo codigo
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Especialidade> buscarPeloCodigo(@PathVariable Long codigo) {
+		Especialidade especialidade = especialidadeRepository.findOne(codigo);
+		
+		// Caso a Especialidade seja Nula ele retorna uma 404 Error
+		return especialidade != null ? ResponseEntity.ok(especialidade) : ResponseEntity.notFound().build();
+
+	}
+
+	// Se meu Json trouxer valores Nulos // 204 Not Content
+	// @GetMapping
+	// public ResponseEntity<?> listar() {
+	// List<Especialidade> especialidades = especialidadeRepository.findAll();
+	// return !especialidades.isEmpty() ? ResponseEntity.ok(especialidades) :
+	// ResponseEntity.noContent().build();
+	// }
 
 }
