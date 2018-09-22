8fc888e5f76faa24fe70284112933731bb36b7cf
Bruno_Santos <brunocontato2012@gmail.com>
2018-09-22 17:24:07 -0300

    FIX1

--- med-api/src/main/java/com/example/med/api/security/util/GeradorSenha.java
+++ med-api/src/main/java/com/example/med/api/security/util/GeradorSenha.java
@@ -8,7 +8,7 @@ public class GeradorSenha {
 
 	public static void main(String[] args) {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		System.out.println(encoder.encode("bruno"));
+		System.out.println(encoder.encode("admin"));
 	}
 	
 }
