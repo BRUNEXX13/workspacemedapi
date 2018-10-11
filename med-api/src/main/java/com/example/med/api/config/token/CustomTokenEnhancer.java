428d41aaa85d41c8e5214372ce1bb731403062b1
Bruno_Santos <brunocontato2012@gmail.com>
2018-10-11 18:46:54 -0300

    Update CustomTokenEnhancer.java

--- med-api/src/main/java/com/example/med/api/config/token/CustomTokenEnhancer.java
+++ med-api/src/main/java/com/example/med/api/config/token/CustomTokenEnhancer.java
@@ -10,7 +10,7 @@ import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 
 import com.example.med.api.security.UsuarioSistema;
 
-
+//Mostrar Usuario Logado na API
 public class CustomTokenEnhancer implements TokenEnhancer {
 
 	@Override
